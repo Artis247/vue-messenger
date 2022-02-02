7b1502ee997303b8a7392a7681160fc4352f3dbd
Artur <75115204+Artis247@users.noreply.github.com>
2022-02-02 23:38:38 +0300

    Update firestore.js

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAS3M_1Po96p_rarssROLVneHEcy_ATPk",
-  authDomain: "v-mess.firebaseapp.com",
-  projectId: "v-mess",
-  storageBucket: "v-mess.appspot.com",
-  messagingSenderId: "762268376657",
-  appId: "1:762268376657:web:ee46289057c07e57a42270"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const connection = firebase.initializeApp(firebaseConfig);
